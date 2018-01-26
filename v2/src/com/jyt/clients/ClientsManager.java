5876d7180cbdee94795183e13b981f1388275554
pengwei <pwalan@foxmail.com>
2018-01-26 14:57:57 +0800

    修改客户端管理器，添加所有用户id的注册

--- v2/src/com/jyt/clients/ClientsManager.java
+++ v2/src/com/jyt/clients/ClientsManager.java
@@ -1,33 +1,38 @@
 package com.jyt.clients;
 
+import java.util.List;
+
+import com.jyt.clients.service.UserInfoService;
 import com.jyt.message.MessageConfig;
 import com.jyt.message.MessageManager;
-import com.jyt.message.example.MyManager;
 
 public class ClientsManager extends MessageManager {
-	public ClientsManager(String server_ip,String server_name)
-	{
-		super(server_ip,server_name);
+	public ClientsManager(String server_ip, String server_name) {
+		super(server_ip, server_name);
 		manager.register("sys_login");
 		manager.register("sys_friends");
 		manager.register("sys_group");
 		manager.register("sys_userinfo");
 		manager.register("sys_search");
 		manager.register("sys_test");
+
+		// 注册所有用户
+		List<String> uids = UserInfoService.getAllUsers();
+		for (String uid : uids) {
+			manager.register(uid);
+		}
 	}
-	
+
 	public static void main(String[] args) {
 		String server_ip = "127.0.0.1";
 
-		MyManager m = new MyManager(server_ip, MessageConfig.server_name);
+		ClientsManager m = new ClientsManager(server_ip, MessageConfig.server_name);
 		// m.build_menu();
-		
-		//TODO 从数据库中读取用户id然后注册到manager
-		
+
 		while (true) {
 			m.input_main();
 		}
 
 	}
-	
+
 }
--- v2/src/com/jyt/clients/service/UserInfoService.java
+++ v2/src/com/jyt/clients/service/UserInfoService.java
@@ -1,5 +1,13 @@
 package com.jyt.clients.service;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.jyt.clients.db.ConnectionPool;
+import com.jyt.clients.db.ConnectionPoolUtils;
 import com.jyt.clients.model.User;
 
 public class UserInfoService {
@@ -10,5 +18,23 @@ public class UserInfoService {
 	
 	public static void modifyUserInfo(User user){
 		
+	}
+	
+	public static List<String> getAllUsers(){
+		ConnectionPool connPool=ConnectionPoolUtils.GetPoolInstance();//单例模式创建连接池对象  
+		List<String> uids=new ArrayList<String>();
+		String sql="select * from user";
+		try {
+			Connection conn = connPool.getConnection();
+			Statement stmt = conn.createStatement();  
+			ResultSet rs = stmt.executeQuery(sql);
+			while(rs.next()){
+				uids.add(rs.getString("id"));
+			}
+			return uids;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return uids;
+		}
 	}
 }
